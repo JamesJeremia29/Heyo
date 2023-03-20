3199ad64433c497acc0b072839483898d838eb3e
James Jeremia <jamesjeremia29@gmail.com>
2023-03-20 21:14:05 +0700

    Initial Commit

---
+++ Heyo/HeyoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeyoApp.swift
+//  Heyo
+//
+//  Created by James Jeremia on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct HeyoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
